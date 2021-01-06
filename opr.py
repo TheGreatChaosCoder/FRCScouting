2fa8bcb5e4ec826c0305de103105ce2ea48faf40
Connor Johnson <thegreatchaoshunter@gmail.com>
2021-01-05 21:30:16 -0600

    shows table of team opr from 2020 gkc

--- opr.py
+++ opr.py
@@ -3,9 +3,17 @@
 
 import requests
 
+baseUrl = 'https://www.thebluealliance.com/api/v3'
 auth_key = 'c2xpZrkWoRAnGNl3fDmXwbg7eKLIdzs0ADRG0xXhqEFyeHOtFcW1PF3jr8qVIcUx'
 headers = {'X-TBA-Auth-Key' : auth_key}
 
-r = requests.get('https://www.thebluealliance.com/api/v3', headers = headers)
-print(r.url)
-print(r.status_code)
+def getTBAJson(url):
+	return requests.get(baseUrl + url, headers=headers).json()
+
+opr = getTBAJson('/event/2020mokc/oprs')
+oprDict = opr["oprs"]
+
+for team in opr["oprs"]:
+
+    print(team+': ' + str(oprDict[team]));
+
